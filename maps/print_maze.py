64cbef1b46f3d863dd18e3022d327bbad6373c53
stevekutz <stkutz@gmail.com>
2020-08-29 17:58:28 -0500

    added room and world

---
+++ maps/print_maze.py
@@ -0,0 +1,22 @@
+from world import World
+from ast import literal_eval
+import random
+
+# Load world
+world = World()
+
+
+# You may uncomment the smaller graphs for development and testing purposes.
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
+
+# Loads the map into a dictionary
+room_graph=literal_eval(open(map_file, "r").read())
+world.load_graph(room_graph)
+
+# Print an ASCII map
+world.print_rooms()
